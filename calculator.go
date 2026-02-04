a9d0990ca68314d43916ad60cb1f74e8675f7c51
fatehullah778866 <fatehmsd2522004@gmail.com>
2026-02-04 14:40:30 +0500

    a simple calculator

---
+++ calculator.go
@@ -0,0 +1,42 @@
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	var num1 int
+	var num2 int
+	var opr string
+	fmt.Print("Enter opr: ")
+	fmt.Scanln(&opr)
+	fmt.Print("Enter num1: ")
+	fmt.Scanln(&num1)
+	fmt.Print("Enter num2: ")
+	fmt.Scanln(&num2)
+
+	switch opr {
+	case "+":
+		var c int = num1 + num2
+		fmt.Println("addition: ", c)
+		break
+	case "-":
+		var d int = num1 - num2
+		fmt.Println("subtraction: ", d)
+		break
+	case "*":
+		var e int = num1 * num2
+		fmt.Println("multiplication: ", e)
+		break
+	default:
+		fmt.Println("Invalid operator")
+	}
+	// 	var c int = num1 + num2
+	// 	var d int = num1 - num2
+	// 	var e int = num1 * num2
+
+	// 	fmt.Println(c)
+	// 	fmt.Println(d)
+	// 	fmt.Println(e)
+
+}
